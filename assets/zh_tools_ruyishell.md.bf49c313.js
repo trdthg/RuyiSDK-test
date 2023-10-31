217597d03583fb6455ff528493b5ec58494e5458
trdthg <trdthg@users.noreply.github.com>
2023-10-31 06:49:14 +0000

    deploy: 765223317a656c81aa3b4cee64ad136a24249431

---
+++ assets/zh_tools_ruyishell.md.bf49c313.js
@@ -0,0 +1,27 @@
+import{_ as s,o as a,c as n,Q as l}from"./chunks/framework.35bd0d1d.js";const E=JSON.parse('{"title":"ruyishell","description":"","frontmatter":{},"headers":[],"relativePath":"zh/tools/ruyishell.md","filePath":"zh/tools/ruyishell.md","lastUpdated":1698734878000}'),p={name:"zh/tools/ruyishell.md"},o=l(`<h1 id="ruyishell" tabindex="-1">ruyishell <a class="header-anchor" href="#ruyishell" aria-label="Permalink to &quot;ruyishell&quot;">​</a></h1><h2 id="功能介绍" tabindex="-1">功能介绍 <a class="header-anchor" href="#功能介绍" aria-label="Permalink to &quot;功能介绍&quot;">​</a></h2><p>ruyishell 是一个辅助开发工具，不需要手动搭建复杂的环境和下载代码，只需要几条命令，就可以直接获取所需要的构建好的软件包。</p><h2 id="命令" tabindex="-1">命令 <a class="header-anchor" href="#命令" aria-label="Permalink to &quot;命令&quot;">​</a></h2><p>ruyishell 目前支持的命令如下：</p><div class="language-bash vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#B392F0;">ruyi</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">on/off</span></span>
+<span class="line"><span style="color:#6A737D;"># 打开/关闭 RuyiSDK 开发环境</span></span>
+<span class="line"><span style="color:#B392F0;">ruyi</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">update</span></span>
+<span class="line"><span style="color:#6A737D;"># 获取 ruyisdk 最新更新</span></span>
+<span class="line"><span style="color:#B392F0;">ruyi</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">install</span><span style="color:#E1E4E8;"> [sdkid]</span></span>
+<span class="line"><span style="color:#6A737D;"># 安装指定的sdk</span></span>
+<span class="line"><span style="color:#B392F0;">ruyi</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">list</span></span>
+<span class="line"><span style="color:#6A737D;"># 罗列本地支持SDK环境,附带默认环境</span></span>
+<span class="line"><span style="color:#B392F0;">ruyi</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">info</span></span>
+<span class="line"><span style="color:#6A737D;"># 罗列本地支持SDK环境</span></span>
+<span class="line"><span style="color:#B392F0;">ruyi</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">open/switch/close</span><span style="color:#E1E4E8;"> [sdkid]</span></span>
+<span class="line"><span style="color:#6A737D;"># 使能/切换指定sdk环境</span></span>
+<span class="line"><span style="color:#B392F0;">ruyi</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">run</span></span>
+<span class="line"><span style="color:#6A737D;"># 运行指定qemu环境</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#6F42C1;">ruyi</span><span style="color:#24292E;"> </span><span style="color:#032F62;">on/off</span></span>
+<span class="line"><span style="color:#6A737D;"># 打开/关闭 RuyiSDK 开发环境</span></span>
+<span class="line"><span style="color:#6F42C1;">ruyi</span><span style="color:#24292E;"> </span><span style="color:#032F62;">update</span></span>
+<span class="line"><span style="color:#6A737D;"># 获取 ruyisdk 最新更新</span></span>
+<span class="line"><span style="color:#6F42C1;">ruyi</span><span style="color:#24292E;"> </span><span style="color:#032F62;">install</span><span style="color:#24292E;"> [sdkid]</span></span>
+<span class="line"><span style="color:#6A737D;"># 安装指定的sdk</span></span>
+<span class="line"><span style="color:#6F42C1;">ruyi</span><span style="color:#24292E;"> </span><span style="color:#032F62;">list</span></span>
+<span class="line"><span style="color:#6A737D;"># 罗列本地支持SDK环境,附带默认环境</span></span>
+<span class="line"><span style="color:#6F42C1;">ruyi</span><span style="color:#24292E;"> </span><span style="color:#032F62;">info</span></span>
+<span class="line"><span style="color:#6A737D;"># 罗列本地支持SDK环境</span></span>
+<span class="line"><span style="color:#6F42C1;">ruyi</span><span style="color:#24292E;"> </span><span style="color:#032F62;">open/switch/close</span><span style="color:#24292E;"> [sdkid]</span></span>
+<span class="line"><span style="color:#6A737D;"># 使能/切换指定sdk环境</span></span>
+<span class="line"><span style="color:#6F42C1;">ruyi</span><span style="color:#24292E;"> </span><span style="color:#032F62;">run</span></span>
+<span class="line"><span style="color:#6A737D;"># 运行指定qemu环境</span></span></code></pre></div>`,6),e=[o];function c(r,t,i,y,d,u){return a(),n("div",null,e)}const F=s(p,[["render",c]]);export{E as __pageData,F as default};
